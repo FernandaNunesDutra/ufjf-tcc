770253646e1edae64afabb965731c39907740a97
Thiago Costa Rizuti da Rocha <t.rizuti@gmail.com>
2016-11-23 04:28:41 -0200

    code cleaning

--- recommender.py
+++ recommender.py
@@ -4,56 +4,28 @@ from sklearn import cluster, datasets, neighbors, metrics, preprocessing, neighb
 from scipy.stats import pearsonr
 from datetime import datetime
 
-
-def readAesUserQuitData():
-    read = np.loadtxt("data_aes_users_quit.csv", delimiter=',', skiprows=1)
+def readUserData(dataset):
+    if (dataset == 'aes'):
+        file_name = "aes_users.csv"
+    if (dataset == 'wati'):
+        file_name = "wati_users.csv"
+    read = np.loadtxt(file_name, delimiter=',', skiprows=1)
+    temp = read.view(np.ndarray)
+    np.lexsort((temp[:, 0], ))
+    read = temp[np.lexsort((temp[:, 0], ))]
     return read[:, 1:], read[:,0].astype(int)
 
-def readAesUserCutData():
-    read = np.loadtxt("data_aes_users_cut.csv", delimiter=',', skiprows=1)
-    return read[:, 1:], read[:,0].astype(int)
-
-def readAesUserData():
-    read = np.loadtxt("data_aes_users.csv", delimiter=',', skiprows=1)
-    return read[:, 1:], read[:,0].astype(int)
-
-def readWatiUserData():
-    read = np.loadtxt("data_wati_users.csv", delimiter=',', skiprows=1)
-    return read[:, 1:], read[:,0].astype(int)
-
-def readAesItemData():
-    read = np.loadtxt("data_aes_ratings.csv", delimiter=',', skiprows=1, dtype='int')
-    matrix = np.zeros((9167,30),dtype=np.int)
-
-    for i,row in enumerate(read):
-        matrix[row[0]][0] = row[0]
-        matrix[row[0]][row[1]] = row[2]
-
-    indexes = []
-    for i, row in enumerate(matrix):
-        if row[0] == 0:
-            indexes.append(i)
-    matrix = np.delete(matrix, indexes, axis=0)
-    matrix = matrix.T
-    return matrix[1:,:], [x for x in range(1,30)]
-
-def readWatiItemData():
-    read = np.loadtxt("data_wati_ratings.csv", delimiter=',', skiprows=1, dtype='int')
-    matrix = np.zeros((1461,10),dtype=np.int)
-    for i,row in enumerate(read):
-        matrix[row[0]][0] = row[0]
-        matrix[row[0]][row[1]] = row[2]
-    indexes = []
-    for i, row in enumerate(matrix):
-        if row[0] == 0:
-            indexes.append(i)
-    matrix = np.delete(matrix, indexes, axis=0)
-    matrix = matrix.T
-    return matrix[1:,:], [x for x in range(1,10)]
-
-def readAesUserItemMatrix():
-    read = np.loadtxt("data_aes_ratings.csv", delimiter=',', skiprows=1, dtype='int')
-    matrix = np.zeros((9167,30),dtype=np.int)
+def readUserItemMatrix(dataset):
+    if (dataset == 'aes'):
+        file_name = "aes_ratings.csv"
+        m = 9167
+        n = 9
+    if (dataset == 'wati'):
+        file_name = "wati_ratings.csv"
+        m = 1461
+        n = 10
+    read = np.loadtxt(file_name, delimiter=',', skiprows=1, dtype='int')
+    matrix = np.zeros((m,n),dtype=np.int)
     for i,row in enumerate(read):
         matrix[row[0]][0] = row[0]
         matrix[row[0]][row[1]] = row[2]
@@ -64,46 +36,14 @@ def readAesUserItemMatrix():
     matrix = np.delete(matrix, indexes, axis=0)
     return matrix[:,1:]
 
-def readAesUserItemQuitMatrix():
-    read = np.loadtxt("data_aes_ratings_quit.csv", delimiter=',', skiprows=1, dtype='int')
-    matrix = np.zeros((9167,9),dtype=np.int)
-    for i,row in enumerate(read):
-        matrix[row[0]][0] = row[0]
-        matrix[row[0]][row[1]] = row[2]
-    indexes = []
-    for i, row in enumerate(matrix):
-        if row[0] == 0:
-            indexes.append(i)
-    matrix = np.delete(matrix, indexes, axis=0)
-    return matrix[:,1:]
+def computeClusters(data,algorithm,clusters,metric):
+    if(algorithm == 'kmeans'):
+        kmeans = cluster.KMeans(n_clusters=clusters)
+        return kmeans.fit_predict(data)
+    if(algorithm == 'agglomerative'):
+        agglomerative = cluster.AgglomerativeClustering(n_clusters=clusters,affinity=metric,linkage='average')
+        return agglomerative.fit_predict(data)
 
-def readWatiUserItemMatrix():
-    read = np.loadtxt("data_wati_ratings.csv", delimiter=',', skiprows=1, dtype='int')
-    matrix = np.zeros((1461,10),dtype=np.int)
-    for i,row in enumerate(read):
-        matrix[row[0]][0] = row[0]
-        matrix[row[0]][row[1]] = row[2]
-    indexes = []
-    for i, row in enumerate(matrix):
-        if row[0] == 0:
-            indexes.append(i)
-    matrix = np.delete(matrix, indexes, axis=0)
-    return matrix[:,1:]
-
-def kmeans(data, clusters):
-    kmeans = cluster.KMeans(n_clusters=clusters)
-    return kmeans.fit_predict(data)
-
-def agglomerative(data, clusters,metric):
-    agglomerative = cluster.AgglomerativeClustering(n_clusters=clusters,affinity=metric,linkage='average')
-    return agglomerative.fit_predict(data)
-
-def dbscan(data,eps,min_samples):
-    dbscan = cluster.DBSCAN(eps=eps, min_samples=min_samples, metric='euclidean',algorithm='brute')
-    return dbscan.fit_predict(data)
-
-def calinskiHarabazScore(data, clustering):
-    return metrics.calinski_harabaz_score(data,clustering)
 
 def silhouetteScore(data,clustering,metric):
     return metrics.silhouette_score(X=data,labels=clustering,metric=metric)
@@ -112,17 +52,17 @@ def scaleData(data):
     scaler = preprocessing.MinMaxScaler()
     return scaler.fit_transform(data)
 
+def similarity(x,y,metric):
+    if(metric == 'manhattan' or metric == 'euclidean'):
+        dist = metrics.pairwise.pairwise_distances(x.reshape(1, -1),y.reshape(1, -1),metric=metric)[0]
+        if dist == 0:
+            return 1;
+        else:
+            return 1/dist
+    else:
+        return metrics.pairwise.pairwise_distances(x.reshape(1, -1),y.reshape(1, -1),metric=metric)[0]
 
-def pearson_affinity(M):
-   return 1 - np.array([[pearsonr(a,b)[0] for a in M] for b in M])
-
-def pearson(x,y):
-    return pearsonr(x,y)[0]
-
-def similarity(x,y):
-    return metrics.pairwise.pairwise_distances(x.reshape(1, -1),y.reshape(1, -1),metric='cosine')[0]
-
-def ratingDensity(nClusters, userClusters, userItemMatrix):
+def raters(nClusters, userClusters, userItemMatrix):
     ratingRatio = []
     for cluster in range(nClusters):
         raterCount = 0
@@ -132,32 +72,53 @@ def ratingDensity(nClusters, userClusters, userItemMatrix):
                 neighborCount += 1
                 if np.sum(ratings) != 0:
                     raterCount += 1
-                #for rating in ratings:
-                #    if rating != 0:
-                #        ratingCount += 1
-                #if ratingCount != 0:
-                #    neighborCount += 1
         ratingRatio.append((raterCount,neighborCount))
     return ratingRatio
 
-def main():
-    random.seed(datetime.now())
-
-    userData, userIndexes = readAesUserQuitData()
-    userItemMatrix = readAesUserItemQuitMatrix()
-    print userData.shape
+def clusterAnalysis(dataset):
+    userData, userIndexes = readUserData(dataset)
     userData = scaleData(userData)
-    nClusters = 2
-    userClusters = agglomerative(userData,2,'cosine')
+
+    print 'KMEANS'
+    for nClusters in range(2,11):
+        userClusters = computeClusters(userData,'kmeans',nClusters,'euclidean')
+        print silhouetteScore(userData, userClusters,"euclidean")
+    print '============================'
+
+    print 'AGGLOMERATIVE EUCLIDEAN'
+    for nClusters in range(2,11):
+        userClusters = computeClusters(userData,'agglomerative',nClusters,'euclidean')
+        print silhouetteScore(userData, userClusters,"euclidean")
+    print '============================'
+
+    print 'AGGLOMERATIVE COSINE'
+    for nClusters in range(2,11):
+        userClusters = computeClusters(userData,'agglomerative',nClusters,'cosine')
+        print silhouetteScore(userData, userClusters,"cosine")
+    print '============================'
+
+    print 'AGGLOMERATIVE MANHATTAN'
+    for nClusters in range(2,11):
+        userClusters = computeClusters(userData,'agglomerative',nClusters,'manhattan')
+        print silhouetteScore(userData, userClusters,"manhattan")
+    print '============================'
+
+def computeRecommendations(dataset,algorithm,nClusters,metric):
+
+    userData, userIds = readUserData(dataset)
+    userItemMatrix = readUserItemMatrix(dataset)
+
+    userData = scaleData(userData)
+
+    userClusters = computeClusters(userData,algorithm,nClusters,metric)
     print userClusters
-    print silhouetteScore(userData, userClusters,"cosine")
-    print ratingDensity(nClusters, userClusters, userItemMatrix)
+    print silhouetteScore(userData, userClusters,metric)
+    print raters(nClusters, userClusters, userItemMatrix)
 
     userSimilarityMatrix = np.empty((userData.shape[0],userData.shape[0],))
-    print userItemMatrix.shape
     for i, u1 in enumerate(userData):
         for j, u2 in enumerate(userData):
-            userSimilarityMatrix[i][j] = similarity(u1,u2)
+            userSimilarityMatrix[i][j] = similarity(u1,u2,metric)
 
     predictions = np.zeros((userItemMatrix.shape[0],userItemMatrix.shape[1],))
 
@@ -172,10 +133,7 @@ def main():
                 if userItemMatrix[userIndex][itemIndex] == 0:
                     ratingSum = 0
                     for neighborIndex in neighbors:
-                        sim = similarity(userData[userIndex],userData[neighborIndex])
-                        #if(sim == 0):
-                        #    ratingSum += userItemMatrix[neighborIndex][itemIndex]
-                        #else:
+                        sim = similarity(userData[userIndex],userData[neighborIndex],metric)
                         ratingSum += userItemMatrix[neighborIndex][itemIndex] * (sim)
                     predictions[userIndex][itemIndex] = ratingSum
         else:
@@ -192,32 +150,22 @@ def main():
 
     for userIndex, userRow in enumerate(predictions):
         recommendations = userRow.argsort()[-3:][::-1]
-        print userIndexes[userIndex],
+        print userIds[userIndex],
         for recommendation in recommendations:
-            if predictions[userIndex][recommendation] != -1:
-                print recommendation + 1,
+            if predictions[userIndex][recommendation] > 0:
+                print recommendation + 1 ,
         print \
 
-    print "RANDOMIZED"
-    for userIndex, userRow in enumerate(predictions):
-        if(userIndex % 2 == 1):
-            recommendations = userRow.argsort()[-3:][::-1]
-            print userIndexes[userIndex],
-            for recommendation in recommendations:
-                if predictions[userIndex][recommendation] != -1:
-                    print recommendation + 1,
-            print \
+def main():
+    random.seed(datetime.now())
 
-    print "RECOMMENDED"
-    for userIndex, userRow in enumerate(predictions):
-        if(userIndex % 2 == 0):
-            recommendations = userRow.argsort()[-3:][::-1]
-            print userIndexes[userIndex],
-            for recommendation in recommendations:
-                if predictions[userIndex][recommendation] != -1:
-                    print recommendation + 1,
-            print \
+    dataset = 'aes'
+    nclusters = 2
+    metric = 'cosine'
+    algorithm = 'agglomerative'
 
+    computeRecommendations(dataset,algorithm,nclusters,metric)
+    #clusterAnalysis('wati')
 
 
 if __name__ == "__main__":
